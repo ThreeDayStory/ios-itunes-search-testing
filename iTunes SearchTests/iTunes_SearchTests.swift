d5c0081d1b3373e921ee1aca6f40504004e497a3
Jessie <jessie.griffin@icloud.com>
2020-04-21 22:31:16 -0400

    Adds Test files

--- iTunes SearchTests/iTunes_SearchTests.swift
+++ iTunes SearchTests/iTunes_SearchTests.swift
@@ -7,27 +7,22 @@
 //
 
 import XCTest
+@testable import iTunes_Search
 
 class iTunes_SearchTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    func testSuccessfulSearch() {
+        
+        let searchResultsController = SearchResultController()
+        searchResultsController.performSearch(for: "Tweetbot", resultType: .software) { result in
+            switch result {
+            case .success(let result):
+//                XCTAssert(result.count > 0)
+                XCTFail("The search function failed")
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
+            case .failure(let error):
+                XCTFail("The search function failed \(error)")
+            }
         }
     }
-
 }
