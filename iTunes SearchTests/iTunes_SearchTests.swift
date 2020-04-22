5c12ef1d1e0aa9bb18088a71a63d65b712533b0d
Jessie <jessie.griffin@icloud.com>
2020-04-21 22:42:01 -0400

    Adds expectation and break point to test performSearch()

--- iTunes SearchTests/iTunes_SearchTests.swift
+++ iTunes SearchTests/iTunes_SearchTests.swift
@@ -13,16 +13,26 @@ class iTunes_SearchTests: XCTestCase {
 
     func testSuccessfulSearch() {
         
+        let searchExpectation = expectation(description: "Waiting for results.")
+        
         let searchResultsController = SearchResultController()
         searchResultsController.performSearch(for: "Tweetbot", resultType: .software) { result in
+            
             switch result {
             case .success(let result):
-//                XCTAssert(result.count > 0)
-                XCTFail("The search function failed")
-
+                XCTAssert(result.count > 0)
+                
             case .failure(let error):
                 XCTFail("The search function failed \(error)")
             }
+            
+            // Continue with the function
+            searchExpectation.fulfill() // happening on another thread
+            
         }
+        
+        // Wait for the results
+        // Blocking the main thread from executing (never ever block the main thread in the actualy app)
+        wait(for: [searchExpectation], timeout: 5)
     }
 }
